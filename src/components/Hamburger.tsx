e6f4afd9e3b402d3ee141689a5cee005408e3b3e
Pooja R <pooja.ramesh@imaginelearning.com>
2023-05-16 15:19:00 +0530

    Navbar in progress and mobile menu

---
+++ src/components/Hamburger.tsx
@@ -0,0 +1,18 @@
+import data from "../data";
+import { Link } from "react-router-dom";
+
+const Hamburger = () => {
+  return (
+    <ul className="flex-col flex items-center">
+      {data.navbar.map((item, index) => (
+        <li className="py-2 w-full text-center" key={index}>
+          <Link to={item.url} className="font-semibold">
+            {item.listname}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Hamburger;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,25 @@
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
 import data from "../data";
 import { Link } from "react-router-dom";
+import Hamburger from "./Hamburger";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+
   return (
     <nav className="p-5">
       <div className="flex w-full justify-between ">
         <div>
-          <Link to="/" className="font-black text-2xl font-sans">
+          <Link
+            to="/"
+            className="font-black text-2xl font-sans hover:underline hover:decoration-4"
+          >
             {data.name}
           </Link>
         </div>
-        <div>
+
+        <div className="hidden md:block">
           <ul className="flex w-full justify-between">
             {data.navbar.map((item, index) => (
               <li
@@ -27,7 +36,17 @@ const Navbar = () => {
             ))}
           </ul>
         </div>
+
+        <div className="md:hidden">
+          <GiHamburgerMenu
+            style={{ width: "30px", height: "30px" }}
+            onClick={() => {
+              setClick(true);
+            }}
+          />
+        </div>
       </div>
+      <div className="bg-gray-400">{click === true && <Hamburger />}</div>
     </nav>
   );
 };
