8ea00c98b77bdac31a82773acbed3d3b2afb9f91
Pooja R <pooja.ramesh@imaginelearning.com>
2023-05-23 11:31:32 +0530

    Project  Responsive done

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -3,6 +3,7 @@ import { BsGithub } from "react-icons/bs";
 import { content } from "../content";
 import Tooltip from "./Tooltip";
 import { tooltipPostitionEnum } from "../config";
+import "../styles/ImageCard.css";
 
 const ImageCard = () => {
   const background = useSelector((state: any) => state.theme.background);
@@ -10,12 +11,12 @@ const ImageCard = () => {
 
   return (
     <div
-      className={`${background} text-center flex items-center justify-center flex-col`}
+      className={`${background} text-center flex flex-wrap items-center justify-center flex-col p-3`}
     >
       {content.projects.projectslist?.map((item: any, index: any) => {
         return (
           <div
-            className={`flex pb-16 items-baseline transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110`}
+            className={`flex flex-wrap pb-16 items-baseline transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 justify-center`}
             key={index}
           >
             <div className={`cursor-pointer`}>
@@ -28,12 +29,11 @@ const ImageCard = () => {
                 <img
                   src={item.proimg}
                   alt=""
-                  className={`rounded-3xl shadow-slate-300 ${
+                  className={`h-auto md:h-96 rounded-3xl shadow-slate-300 proj-img ${
                     text === "text-white"
                       ? "shadow-xl"
                       : "shadow-2xl border-black border-2"
                   }`}
-                  style={{ height: "400px" }}
                 />
               </a>
             </div>
@@ -44,15 +44,15 @@ const ImageCard = () => {
               content={"Code Link"}
             >
               <div
-                className={`border-2 rounded-xl p-4 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 basis-1/2 ${
+                className={`border-2 rounded-xl py-2 px-3 md:p-4 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 basis-1/2 ${
                   text === "text-white"
                     ? "bg-white text-black"
                     : "bg-black text-white"
                 }`}
               >
-                <div className={`flex`}>
+                <div className={`flex flex-wrap items-center`}>
                   <a
-                    className="text-xl font-bold pr-3 hover:underline underline-offset-4 decoration-2"
+                    className="text-base md:text-xl font-bold pr-3 hover:underline underline-offset-4 decoration-2"
                     href={item.gitlink}
                     target="_blank"
                     rel="noreferrer"
@@ -61,8 +61,8 @@ const ImageCard = () => {
                   </a>
 
                   <BsGithub
-                    style={{ width: "25px", height: "25px" }}
-                    className=""
+                    // style={{ width: "25px", height: "25px" }}
+                    className="w-5 h-5 md:w-6 md:h-6"
                   />
                 </div>
               </div>
