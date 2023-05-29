38a78854beb70c17e711c20b15cc95b59c7341e7
Pooja R <pooja.ramesh@imaginelearning.com>
2023-05-29 17:12:54 +0530

    Removed few commented code

--- src/screens/Experiences.tsx
+++ src/screens/Experiences.tsx
@@ -1,14 +1,9 @@
-// import Heading from "../components/Heading";
 import PageNotFound from "../components/PageNotFound";
 import { content } from "../content";
 
 const Experiences = () => {
   return (
     <>
-      {/* <Heading
-        heading={content.experiences.heading}
-        subHeading={content.experiences.sunheading}
-      /> */}
       <PageNotFound pageName={content.experiences.heading} />
     </>
   );
